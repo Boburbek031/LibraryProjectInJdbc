94e21fa364e9f33b6069da447cb3bb282f8695b7
Boburjon <raximovboburjon66@gmail.com>
2024-02-05 15:47:04 +0500

    Optimization of the project wholly started. CompoundContainer class optimized

--- src/main/java/uz/ali/container/CompoundContainer.java
+++ src/main/java/uz/ali/container/CompoundContainer.java
@@ -5,17 +5,14 @@ import uz.ali.model.Profile;
 import uz.ali.repository.*;
 import uz.ali.service.*;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class CompoundContainer {
 
-
     public static Scanner scannerNum = new Scanner(System.in);
     public static Scanner scannerStr = new Scanner(System.in);
+
+    // Controllers
     public static AdminController adminController = new AdminController();
     public static StudentController studentController = new StudentController();
     public static StaffController staffController = new StaffController();
@@ -24,19 +21,26 @@ public class CompoundContainer {
     public static ProfileController profileController = new ProfileController();
     public static StudentProfileController studentProfileController = new StudentProfileController();
     public static MainController mainController = new MainController();
+
+    // Services
     public static InitService initService = new InitService();
     public static AuthService authService = new AuthService();
     public static CategoryService categoryService = new CategoryService();
     public static BookService bookService = new BookService();
     public static ProfileService profileService = new ProfileService();
     public static StudentBookService studentBookService = new StudentBookService();
+
+    // Repositories
     public static ProfileRepository profileRepository = new ProfileRepository();
     public static CategoryRepository categoryRepository = new CategoryRepository();
     public static BookRepository bookRepository = new BookRepository();
     public static StudentBookRepository studentBookRepository = new StudentBookRepository();
     public static TableRepository tableRepository = new TableRepository();
 
-    // bu project da bitta odam ishlatgani uchun buni static qilib ishlatsek bolaveradi, lekin real project larda
-    // bunday bo'lmaydi, ularda bir vaqtni o'zida bir nechta user kirib ishlatadi
+
+    /* bu project da bitta odam ishlatgani uchun buni static qilib ishlatsek bolaveradi, lekin real project larda
+     bunday bo'lmaydi, ularda bir vaqtni o'zida bir nechta user kirib ishlatadi*/
+    // User
     public static Profile currentProfile;
+
 }
